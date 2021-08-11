45d76478270e7f0b1d1df93785be4e16a951326c
NataZG <86071128+NataZG@users.noreply.github.com>
2021-08-11 20:19:39 +0300

    Swimat

--- HW25_RealmToDo/HW25_RealmToDo/Controllers/TasksViewController.swift
+++ HW25_RealmToDo/HW25_RealmToDo/Controllers/TasksViewController.swift
@@ -92,6 +92,8 @@ class TasksViewController: UITableViewController {
             tappedItem.isComplete = !tappedItem.isComplete }
         tableView.reloadData()
     }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
